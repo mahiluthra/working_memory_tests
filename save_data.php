5ec1f16f5f3a1b2a4e495c0081736a999f209544
Mahi Luthra <mahi.luthra@gmail.com>
2020-10-02 11:35:39 -0400

    php script

---
+++ save_data.php
@@ -0,0 +1,8 @@
+<?php
+// the $_POST[] array will contain the passed in filename and data
+// the directory "data" is writable by the server (chmod 777)
+$filename = "data/".$_POST['filename'];
+$data = $_POST['filedata'];
+// write the file to disk
+file_put_contents($filename, $data);
+?>
